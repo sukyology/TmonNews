6508d1870bc551f944c706e59ee223688a8d45d6
koyk091302 <koyk091302@tmon-koyk091302.tmoncorp.com>
2014-05-21 15:05:09 +0900

    SocialCategoryBO 모두 비워진것

--- TmonNews/src/main/java/kr/co/tmon/socialnews/bo/SocialCategoryBO.java
+++ TmonNews/src/main/java/kr/co/tmon/socialnews/bo/SocialCategoryBO.java
@@ -1,19 +1,5 @@
 package kr.co.tmon.socialnews.bo;
 
-import java.util.List;
-
-import kr.co.tmon.socialnews.model.News;
-
 public class SocialCategoryBO {
 
-	public List<News> getNewsList() {
-		return null;
-	}
-
-	public void selectedOtherPage(int page, String selectedSocialCorporation, String selectedDate) {
-	}
-
-	public void selectedAllCorporation(int page, String selectedDate) {
-
-	}
 }
