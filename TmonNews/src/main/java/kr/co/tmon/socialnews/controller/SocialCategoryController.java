903cf201058aa599a7d4d39a73e23252cf4d2d75
koyk091302 <koyk091302@tmon-koyk091302.tmoncorp.com>
2014-05-21 16:03:18 +0900

    socialCategoryController

--- TmonNews/src/main/java/kr/co/tmon/socialnews/controller/SocialCategoryController.java
+++ TmonNews/src/main/java/kr/co/tmon/socialnews/controller/SocialCategoryController.java
@@ -1,28 +1,62 @@
 package kr.co.tmon.socialnews.controller;
+/**
+ * 
+ * @author 고영경
+ * 
+ */
+import java.util.Date;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+
+import kr.co.tmon.socialnews.bo.SocialCategoryBO;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
-public class SocialCategoryController {
-	@RequestMapping("/social")
-	public ModelAndView ControlAllSocialNews(int page) {
-		return null;
-	}
+@RequestMapping("/category")
+public class SocialCategoryController extends HttpServlet{
+	@Autowired
+	private SocialCategoryBO socialCategoryBO;
 	
+	@RequestMapping("/socials")
+	public ModelAndView ControlAllSocialNews(HttpSession session) {
+		return settingModelAndView("socials", session);
+	}
+
 	@RequestMapping("/coupang")
-	public ModelAndView ControlCoupangNews(int page) {
-		return null;
+	public ModelAndView ControlCoupangNews(HttpSession session) {
+		return settingModelAndView("coupang", session);
 	}
-	
+
 	@RequestMapping("/tmon")
-	public ModelAndView ControlTmonNews(int page) {
-		return null;
+	public ModelAndView ControlTmonNews(HttpSession session) {
+		return settingModelAndView("tmon", session);
+	}
+
+	@RequestMapping("/wemap")
+	public ModelAndView ControlWemapNews(HttpSession session) {
+		return settingModelAndView("wemap", session);
+	}
+
+	private ModelAndView settingModelAndView(String socialCorpName, HttpSession session) {
+		ModelAndView socialCorpModelAndView = new ModelAndView();
+		
+		session.setAttribute("selectedSocialCorp", socialCorpName);
+		
+		socialCategoryBO.setNewsDate((Date) session.getAttribute("selectedDate"));
+		socialCategoryBO.setSocialCorpCode(socialCorpName);
+		
+		socialCorpModelAndView.addObject("newsList", socialCategoryBO.getNewsList());
+		
+		if(socialCorpName.equals("socialCorpName") == false)
+			socialCorpModelAndView.setViewName("newspage");
+		
+		return socialCorpModelAndView;
 	}
 	
-	@RequestMapping("/wemap")
-	public ModelAndView ControlWemapNews(int page) {
-		return null;
-	}
 }
