820f55cef0a43e48bd644fbe3414b6a700cd2df2
pirothewriter <pirothewriter@TMON-PIROTHEWRI.tmoncorp.com>
2014-05-21 15:17:01 +0900

    SocialCategoryBO 클래스의 field와 method 생성

--- TmonNews/src/main/java/kr/co/tmon/socialnews/bo/SocialCategoryBO.java
+++ TmonNews/src/main/java/kr/co/tmon/socialnews/bo/SocialCategoryBO.java
@@ -1,5 +1,26 @@
 package kr.co.tmon.socialnews.bo;
 
-public class SocialCategoryBO {
+import java.util.Date;
+import java.util.List;
 
+import kr.co.tmon.socialnews.model.News;
+
+public class SocialCategoryBO {
+	private List<News> newsList;
+	private String socialCorpCode;
+	private Date newsDate;
+
+	public List<News> getNewsList() {
+		return null;
+	}
+
+	public void setSocialCorpCode(String socialCorpCode) {
+		this.socialCorpCode = socialCorpCode;
+	}
+
+	public void setNewsDate(Date newsDate) {
+		this.newsDate = newsDate;
+	}
+	
+	
 }
