b1b27d85dcdab86e0dc45f9a3eaeafbc1ba3f4df
yeseul23 <yeseul23@tmon-yeseul23.tmoncorp.com>
2014-05-27 20:57:52 +0900

    [NEWOJT-78][신예슬][Xml파싱 BO,DAO구현] RemoteGetNewsDAO, XmlParsingBO(미완), Model(ParsedNews,SocialNewsDocument)구현 dummy.xml(resource)추가 (SocialNewsDocumentTest부분에서 Infinitest버그로 깨집니다. 프로그램상 문제 없습니다.)

---
+++ TmonNews/src/main/java/kr/co/tmon/socialnews/dao/RemoteGetNewsDAO.java
@@ -0,0 +1,31 @@
+package kr.co.tmon.socialnews.dao;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.springframework.stereotype.Repository;
+
+import kr.co.tmon.socialnews.model.SocialNewsDocument;
+/**
+ * 
+ * @author yeseul23
+ *
+ */
+@Repository
+public class RemoteGetNewsDAO {
+	private SocialNewsDocument socialNewsDocument;
+
+	public SocialNewsDocument getSocialNewsDocument() throws MalformedURLException, JAXBException {
+
+		URL url = new URL("http://10.5.220.81:8080/social/news.xml");
+		JAXBContext context = JAXBContext.newInstance(SocialNewsDocument.class);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		socialNewsDocument = (SocialNewsDocument) unmarshaller.unmarshal(url);
+
+		return socialNewsDocument;
+	}
+}
