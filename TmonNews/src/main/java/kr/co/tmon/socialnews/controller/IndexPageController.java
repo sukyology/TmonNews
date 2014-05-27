eb7d4935f6910ab5a4bdcbae0f928088fbaf33a0
koyk091302 <koyk091302@tmon-koyk091302.tmoncorp.com>
2014-05-27 18:18:33 +0900

    [jira-tbd][고영경][controller 와 jsp 연동]  사이트 최초 접속시  dispatcherservlet 거쳐  maincontroller 통해  sitemesh 적용하여 main페이지 작동 확인

---
+++ TmonNews/src/main/java/kr/co/tmon/socialnews/controller/IndexPageController.java
@@ -0,0 +1,14 @@
+package kr.co.tmon.socialnews.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexPageController {
+
+	@RequestMapping("index")
+	public String ControlCalendar() {
+		return "Main";
+	}
+}
